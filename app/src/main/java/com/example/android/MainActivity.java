9feb8a777035ebe0f0f9e24fc4b55b4d5f583b91
liewmh0077 <liewmh-wm19@student.tarc.edu.my>
2020-06-30 23:01:45 +0800

    commit by minhui

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -2,21 +2,30 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.graphics.Color.rgb;
 
 public class MainActivity extends AppCompatActivity {
 
     private int mCount=0;
     private TextView mShowCount;
+    private Button zero;
+    private Button count;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount=(TextView) findViewById(R.id.show_count);
+        zero=findViewById(R.id.button_zero);
+        count=findViewById(R.id.button_count);
     }
 
     public void showToast(View view) {
@@ -28,7 +37,17 @@ public class MainActivity extends AppCompatActivity {
         mCount++;
         if(mShowCount !=null)
             mShowCount.setText(Integer.toString(mCount));
+            zero.setBackgroundColor(Color.GREEN);
+            count.setBackgroundColor(Color.MAGENTA);
 
+
+    }
+
+    public void callMethod(View view) {
+        mCount = 0;
+        mShowCount.setText(Integer.toString(mCount));
+        zero.setBackgroundColor(0xffaaaaaa);
+        count.setBackgroundColor(rgb(98,0,238));
     }
 
 
